1635a602814ff2b7e30c96d26d79eb75af9d43dd
El1syum <kilmol606@gmail.com>
2023-01-26 20:56:04 +0300

    some changes

--- auth.py
+++ auth.py
@@ -1,3 +1,5 @@
+MAIN_URL = 'https://www.truckscout24.de/transporter/gebraucht/kuehl-iso-frischdienst/renault'
+
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
 }
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os.path
 import requests
 from bs4 import BeautifulSoup
 
-from auth import params, headers
+from auth import params, headers, MAIN_URL
 
 
 def save_to_html(html, file_name):
@@ -12,30 +12,35 @@ def save_to_html(html, file_name):
         file.write(html)
 
 
-def main():
+def get_data():
     result = []
-    if not os.path.exists('data/'):
+    i = 1
+    if not os.path.exists('data/'):  # создание директории data
         os.mkdir('data')
-    # save_to_html(response.text, 'index')
-    for i in range(1, 5):
-        if not os.path.exists(f'data/{i}/'):
-            os.mkdir(f'data/{i}')
+
+    while True:  # основной цикл
         params['currentpage'] = i
-        response = requests.get(
-            'https://www.truckscout24.de/transporter/gebraucht/kuehl-iso-frischdienst/renault',
-            params=params,
-            headers=headers,
-        )
+        response = requests.get(MAIN_URL, params=params, headers=headers)  # получение ответа от страницы MAIN_URL из файла auth
         soup = BeautifulSoup(response.text, 'lxml')
+
         card = soup.find('div', class_='ls-elem')
+        if not card:  # проверка на наличие объявлений на странице
+            print('[INFO] Data collected.')
+            break
+
+        if not os.path.exists(f'data/{i}/'):  # создание директорий под каждый id
+            os.mkdir(f'data/{i}')
+
         title = card.find('h2', class_='ls-makemodel').text
-        href = 'https://www.truckscout24.de'+card.find('a', {'data-item-name': 'detail-page-link'}).get('href')
+        href = 'https://www.truckscout24.de' + card.find('a', {'data-item-name': 'detail-page-link'}).get('href')
+
         try:
             price = int(card.find('span', class_='sc-highlighter-4').text.strip()[2:-2].replace('.', '').strip())
         except ValueError:
             price = 0
-        r = requests.get(href, headers=headers)
-        soup = BeautifulSoup(r.text, 'lxml')
+
+        href_response = requests.get(href, headers=headers)  # получение ответа от страницы объявления (href)
+        soup = BeautifulSoup(href_response.text, 'lxml')
 
         try:
             power = int(soup.find('div', text='Leistung').find_next_sibling('div').text.split('kW')[0].strip())
@@ -48,27 +53,30 @@ def main():
             color = ''
 
         try:
-            mileage = int(soup.find('div', text='Kilometer').find_next_sibling('div').text.replace('km', '').replace('.', '').strip())
+            mileage = int(
+                soup.find('div', text='Kilometer').find_next_sibling('div').text.replace('km', '').replace('.',
+                                                                                                           '').strip())
         except AttributeError:
             mileage = 0
 
         try:
-            desc = soup.find('div', class_='short-description').text.replace(' ', '').replace('\r', '')\
+            desc = soup.find('div', class_='short-description').text.replace(' ', '').replace('\r', '') \
                 .replace('\n', ' ').strip()
         except AttributeError:
             desc = ''
 
         try:
-            images = soup.find('div', class_='as24-carousel__container').find_all('div', class_='as24-carousel__item')[:3]
+            images = soup.find('div', class_='as24-carousel__container').find_all('div', class_='as24-carousel__item')[
+                     :3]  # отбор первых трех изображений
             for img_i, image in enumerate(images):
                 src = image.find('img').get('data-src')
                 image_r = requests.get(src)
-                with open(f'data/{i}/{img_i}.jpg', 'wb') as file:
+                with open(f'data/{i}/{img_i}.jpg', 'wb') as file:  # сохранение изображений
                     file.write(image_r.content)
         except AttributeError:
-            print(f'no images {href}')
+            print(f'[ERROR] No images {href}.')
 
-        result.append(
+        result.append(  # наполнение списка собранной информацией
             {
                 'id': i,
                 'href': href,
@@ -80,10 +88,11 @@ def main():
                 'description': desc
             }
         )
+        i += 1  # увеличение i (номера страницы)
 
-    with open('data/data_final.json', 'w', encoding='utf-8') as file:
+    with open('data/data_final.json', 'w', encoding='utf-8') as file:  # сохранение
         json.dump(result, file, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    main()
+    get_data()
